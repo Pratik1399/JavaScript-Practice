ab7950422491e2d63b18d4eda2a9aea47964a649
Pratik Ghadge <pratik.ghadge002@gmail.com>
2019-05-26 23:37:49 +0530

    JS Versions

--- script.js
+++ script.js
@@ -243,7 +243,7 @@
 
 // console.log("Is Mark's BMI higher than John's? " + markHigherBMI);
 
-/*If/else Statement*/
+/*6)If/else Statement*/
 // var firstName = "John";
 
 // var civilStatus = "Single";
@@ -280,7 +280,7 @@
 //   console.log("John's BMI is higher than Mark's");
 // }
 
-/*Boolean Logic*/
+/*7)Boolean Logic*/
 // var firstName = "John";
 
 // var age = 16;
@@ -339,7 +339,7 @@
 //   console.log(firstName + " is a man.");
 // }
 
-/*The Ternary Operator and Switch Statements*/
+/*8)The Ternary Operator and Switch Statements*/
 
 //a)Ternary Operator
 // var firstName = "John";
@@ -399,7 +399,7 @@
 //     console.log(firstName + " is a man.");
 // }
 
-/*Truthy and Falsy values and Equality Operator*/
+/*9)Truthy and Falsy values and Equality Operator*/
 
 //1)Truthy values:NOT falsy values.
 
@@ -439,3 +439,86 @@
 // if (height === "23") {
 //   console.log("The === operator does type coercion!");
 // }
+
+/*10)Coding Challenge*/
+//John and Mike both play basketball in different teams.
+//In the latest 3 games,John's team scored 89,120 and 103 points while Mike's team scored 116,94 and 123 points.
+//1)Calculate the average score for each team.
+//2)Decide which teams wins in average (highest average score),and print thee winner to the console.
+//3)Then change the scores to show different winners.
+//4)Don't forget to take into account there might be a draw(the same average score).
+//5)EXTRA:Mary also plays basketball,and her team scored 97,134 and 105 points.
+//6)Like before,log the average winner to the console.
+//7)HINT:You will need the && operator to take the decision.
+//8)If you can't solve this one,just watch the solution,it's no problem :)
+//9)Like before,change the scores to generate different winners,keeping in mind there might be draws.
+
+//Solution
+
+// var scoreJohn = (89 + 120 + 103) / 3;
+
+// var scoreMike = (116 + 94 + 123) / 3;
+
+// var scoreMary = (97 + 134 + 105) / 3;
+
+// console.log(scoreJohn, scoreMike, scoreMary);
+
+// if (scoreJohn > scoreMike) {
+//     console.log("John\'s team wins with " + scoreJohn + " points");
+// } else if (scoreMike > scoreJohn) {
+//     console.log("Mike\'s team wins with " + scoreMike + " points");
+// } else {
+//     console.log("There is a draw");
+// }
+
+// if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+//   console.log("John's team wins with " + scoreJohn + " points");
+// } else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+//   console.log("Mike's team wins with " + scoreMike + " points");
+// } else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+//   console.log("Mary's team wins with " + scoreMary + " points");
+// } else {
+//   console.log("There is a draw");
+// }
+
+/*11)Functions*/
+
+// function calculateAge(birthYear) {
+//   return 2018 - birthYear;
+// }
+
+// var ageJohn = calculateAge(1990);
+
+// var ageMike = calculateAge(1948);
+
+// var ageJane = calculateAge(1969);
+
+// console.log(ageJohn, ageMike, ageJane);
+
+// function yearUntilRetirement(year,firstName){
+//   var age = calculateAge(year);
+//   var retirement= 65 - age;
+//   console.log(firstName + ' retires in ' + retirement + ' years.');
+// }
+
+// yearUntilRetirement(1990,'John');
+
+// yearUntilRetirement(1948,'Mike');
+
+// yearUntilRetirement(1969,'Jane');
+
+// function yearUntilRetirement(year, firstName) {
+//   var age = calculateAge(year);
+//   var retirement = 65 - age;
+//   if (retirement > 0) {
+//     console.log(firstName + " retires in " + retirement + " years.");
+//   } else {
+//     console.log(firstName + " is already retired.");
+//   }
+// }
+
+// yearUntilRetirement(1990, "John");
+
+// yearUntilRetirement(1948, "Mike");
+
+// yearUntilRetirement(1969, "Jane");
